df784dba2fcda567e844404592bc3c15fd2f2064
lovesosas <luisornelas1126@gmail.com>
2023-11-04 19:07:11 -0700

    HTML for home, student, teacher

--- CSE106/Lab_08/Source/app.py
+++ CSE106/Lab_08/Source/app.py
@@ -1,1 +1,14 @@
-print('hello world')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('\Users\Luis\Documents\Code\CSE106\Lab_08\HTML\index.html')
+
+@app.route('/myStudentCourses')
+def about():
+    return render_template('\Users\Luis\Documents\Code\CSE106\Lab_08\HTML\student\myStudentCourses.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
